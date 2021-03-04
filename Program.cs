79adbe87a4df8a77728044d537a3fb732779e16f
Yaviena <magdalena.szafranska@outlook.com>
2021-03-04 10:15:48 +0100

    Refaktoryzacja kodu z zadaniem 2 do metody poza Main

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ namespace WyprawaNa8k
     {
         static void Main(string[] args)
         {
+            Assignment_2();
+        }
+        static void Assignment_2()
+        {
             string hello = "Hej ekipa!";
             Console.WriteLine(hello);
             hello = hello.Replace("Hej", "Siema");
@@ -18,22 +22,21 @@ namespace WyprawaNa8k
 
             // Assignment 2
             string greeting = "Hej Ekipa z Kursu 8k.";
+
             Console.WriteLine(greeting.StartsWith("Hej"));
             Console.WriteLine(greeting.EndsWith("8k."));
             Console.WriteLine();
 
             // Playground with String methods
-
             string name = "Name: Magda";
             string friendName = "Kukuczka";
 
             Console.WriteLine($"Hi, my name is {name} and this is my friend {friendName}!");
             Console.WriteLine($"The name {name} has {name.Length} letters.");
             Console.WriteLine($"The name {friendName} has {friendName.Length} letters.");
-            Console.WriteLine($"Method Substring makes {name.Substring(name.IndexOf(": ")+2)}");
+            Console.WriteLine($"Method Substring makes {name.Substring(name.IndexOf(": ") + 2)}");
             Console.WriteLine($"Method Contains makes {name.Contains("a")}");
             Console.WriteLine($"Method Replace makes {name.Replace("a", "A")}");
-
         }
     }
 }
